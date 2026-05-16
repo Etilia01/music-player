40d01fe62178c7abeeae61bf3550d8fe99bd0fd1
Etilia01 <alexandridisjulia@gmail.com>
2026-05-16 20:15:19 +0200

    you can now start and stop music at will, wooow

--- main.py
+++ main.py
@@ -8,16 +8,26 @@ bg_color = "#313244"
 window = Tk()
 window.geometry("400x400")
 window.title("Music :3")
-#window.iconbitmap("images/wedonthaveiconsyetXD")
+icon_image = tkinter.PhotoImage(file="images/icon.png")
+window.iconphoto(False, icon_image)
 window.configure(background = bg_color)
 
 
 mixer.init()
+mainframe = Frame (
+height= 200,
+width= 300
+)
+mainframe.place(relx=0.5, rely=0.5, anchor=CENTER)
 def open_window():
     filename = fd.askopenfilename()
     mixer.music.load(filename)
     mixer.music.set_volume(0.3)
+    
+def play():
     mixer.music.play()
+def pause():
+    mixer.music.pause()
 small_button = tkinter.Button(
     text="Open File", 
     font=("Arial", 10),  
@@ -28,6 +38,29 @@ small_button = tkinter.Button(
 )
 
 small_button.pack(anchor=NW)
+play_button = tkinter.Button(
+    mainframe,
+    text="Play", 
+    font=("Arial", 10),  
+    padx=2,         
+    pady=2,             
+    command=play,
+    
+)
+play_button.pack(side=LEFT, padx=2)
+
+pause_button = tkinter.Button(
+    mainframe,
+    text="Pause", 
+    font=("Arial", 10),  
+    padx=2,         
+    pady=2,             
+    command=pause,
+    
+)
+pause_button.pack(side=RIGHT, padx=2)
+
+
 window.mainloop()
 
 #things ill need later for this/will implement once i get the buttons working/in there, compiled here so that i dont have to spend hours looking at docs every time i need to figure something like this out
