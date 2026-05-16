4ee91dfbb3cbdb5a89f29162f4213bb32aa5c3af
Etilia01 <alexandridisjulia@gmail.com>
2026-05-16 17:58:56 +0200

    Basic Window done omg

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import tkinter.messagebox
+from tkinter import filedialog
+from pygame import mixer
+import os
+
+bg_color = "#313244"
+window = Tk()
+window.geometry("400x400")
+window.title("Music :3")
+#window.iconbitmap("images/wedonthaveiconsyetXD")
+window.configure(background = bg_color)
+
+window.mainloop()
